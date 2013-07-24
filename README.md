c1890a48cf916b69ab52b2cbae136efe5c942fd8
Patrik Majer <patrik.majer@atlas.cz>
2013-07-24 14:23:35 +0200

    first version

--- README.md
+++ README.md
@@ -1,17 +1,23 @@
 # Zabbix Agent Puppet Module
 This module manages the zabbix agent for a monitored machine.
 
-This module has been tested against Puppet 3.0.1 on Windows Server 2008R2 and Ubuntu Server 12.04.
+This module has been originally tested against Puppet 3.0.1 on Windows Server 2008R2, Ubuntu Server 12.04, and CentOS 6.3.
 
 ### Authors
 * Scott Smerchek <scott.smerchek@softekinc.com>
 
+### Contributors
+
+* Patrik Majer (@czhujer) <patrik.majer.pisek@gmail.com>    
+    
 ## Usage
 
 ```puppet
 class { 'zabbixagent':
   servers  => 'zabbix.example.com', # Optional: defaults to localhost (accepts an array)
+  active_servers  => 'zabbix.example.com', # Optional: defaults to localhost (accepts an array)
   hostname => 'web01.example.com' # Optional: defaults to the hostname of the machine
+  version => '2' #choose version of zabbix-agent: 1 or 2
 }
 ```
 
--- manifests/init.pp
+++ manifests/init.pp
@@ -13,40 +13,193 @@
 class zabbixagent(
   $servers = '',
   $hostname = '',
+  $version = '',
+  $active_servers = '',
 ) {
   $servers_real = $servers ? {
     ''      => 'localhost',
     default => $servers,
   }
+
+  $active_servers_real = $active_servers ? {
+    ''      => 'localhost',
+    default => $active_servers,
+  }
+
   $hostname_real = $hostname ? {
     ''      => $::fqdn,
     default => $hostname,
   }
 
+  $version_real = $version ? {
+    ''      => '1',
+    default => $version,
+  }
+
+  #
+  # check os and version
+  #
+  if $::operatingsystem == "centos" {
+  
+    $os_release = $::operatingsystemrelease
+    
+    $msg_untestedrelease = "Untested version ($os_release) of operating system..."
+    
+    $msg_testedrelease = "Tested on this version ($os_release) of operating system... OK"
+    
+    $msg_testedos = "Unested operating system..."
+    
+    if( $os_release >= 5 and $os_release < 5.9) {
+	notice "Tested on a newer version of operating system than this ($os_release). Please update your OS."
+    }
+    elsif( $os_release >= 5 and $os_release < 6) {
+	notice $msg_testedrelease
+    }
+    else {
+         notice $msg_untestedrelease
+    }
+  }
+  else{
+     notice $msg_untestedos
+  }
+
+  #
+  #  install and resolve package(s)
+  #
+  
   case $::operatingsystem {
     centos: {
       include epel
 
-      package {'zabbix-agent' :
-        ensure  => installed,
-        require => Yumrepo["epel"]
-      }
+      if $version_real == 2 {
+
+        $package = "zabbix20-agent"
+
+        package {"zabbix-agent" :
+           ensure  => absent
+        }
+        package {"zabbix" :
+           ensure  => absent
+        }
+
+        package {"$package" :
+          ensure  => installed,
+          require => Yumrepo["epel"]
+        } 
+       }
+       elsif $version_real == 1 {
+
+        $package = "zabbix-agent"
+
+        package {"zabbix20-agent" :
+           ensure  => absent
+        }
+        package {"zabbix20" :
+           ensure  => absent
+        }
+
+        package {"$package" :
+          ensure  => installed,
+          require => Yumrepo["epel"]
+        } 
+     
+       }
+       else {
+
+         notice "Unsupported version  ${version}, choose 1 or 2."
+       }
+
     }
 
     debian, ubuntu: {
-      package {'zabbix-agent' :
-        ensure  => installed
-      }
-    }
-  }
 
+      if $version_real == 2 {
+
+        notice "I can't install version ${version} of zabbix-agent."
+        notice "exiting..."
+      
+      }
+      elsif $version_real == 1 {
+      
+        $package = "zabbix-agent"
+
+        package {$package :
+    	    ensure  => installed
+        }
+    
+      }
+      else {
+
+         notice "Unsupported version  ${version}, choose 1 or 2."
+      }
+  
+     }
+    }
+
+  #
+  #  modify configuration files
+  #
+  
   case $::operatingsystem {
-    debian, ubuntu, centos: {
+  
+    centos: {
+    
+      ini_setting { 'servers setting':
+        ensure  => present,
+        path    => '/etc/zabbix/zabbix_agentd.conf',
+        section => '',
+        setting => 'Server',
+        value   => join(flatten([$servers_real]), ','),
+        subscribe => Package[$package],
+      }
+    
+      if $version_real == 2 {
+
+        ini_setting { 'active servers setting':
+         ensure  => present,
+         path    => '/etc/zabbix/zabbix_agentd.conf',
+         section => '',
+         setting => 'ServerActive',
+         value   => join(flatten([$active_servers_real]), ','),
+         subscribe => Package[$package],
+        }
+
+      }
+
+      ini_setting { 'hostname setting':
+        ensure  => present,
+        path    => '/etc/zabbix/zabbix_agentd.conf',
+        section => '',
+        setting => 'Hostname',
+        value   => $hostname_real,
+        subscribe => Ini_setting['servers setting'],
+      }
+    
+      ini_setting { 'Include setting':
+        ensure  => present,
+        path    => '/etc/zabbix/zabbix_agentd.conf',
+        section => '',
+        setting => 'Include',
+        value   => '/etc/zabbix/zabbix_agentd/',
+        subscribe => Ini_setting['hostname setting'],
+      }
+
+      file { '/etc/zabbix/zabbix_agentd':
+        ensure  => directory,
+        subscribe => Ini_setting['Include setting'],
+      }
+
       service {'zabbix-agent' :
         ensure  => running,
         enable  => true,
-        require => Package['zabbix-agent'],
+        subscribe => File['/etc/zabbix/zabbix_agentd'],    
       }
+
+    
+    }
+    
+    debian, ubuntu: {
+
 
       ini_setting { 'servers setting':
         ensure  => present,
@@ -54,6 +207,18 @@ class zabbixagent(
         section => '',
         setting => 'Server',
         value   => join(flatten([$servers_real]), ','),
+      }
+
+      if $version_real == 2 {
+
+        ini_setting { 'active servers setting':
+         ensure  => present,
+         path    => '/etc/zabbix/zabbix_agentd.conf',
+         section => '',
+         setting => 'ServerActive',
+         value   => join(flatten([$active_servers_real]), ','),
+        }
+
       }
 
       ini_setting { 'hostname setting':
@@ -75,6 +240,13 @@ class zabbixagent(
       file { '/etc/zabbix/zabbix_agentd':
         ensure  => directory
       }
+
+      service {'zabbix-agent' :
+        ensure  => running,
+        enable  => true,
+        require => Package[$package],
+      }
+
     }
     windows: {
       $confdir = 'C:/ProgramData/Zabbix'
@@ -95,6 +267,15 @@ class zabbixagent(
         require => File["${confdir}/zabbix_agentd.conf"],
       }
 
+        ini_setting { 'active servers setting':
+         ensure  => present,
+         path    => "${confdir}/zabbix_agentd.conf",
+         section => '',
+         setting => 'ServerActive',
+         value   => join(flatten([$active_servers_real]), ','),
+         require => File["${confdir}/zabbix_agentd.conf"],
+        }
+
       ini_setting { 'hostname setting':
         ensure  => present,
         path    => "${confdir}/zabbix_agentd.conf",
@@ -103,7 +284,7 @@ class zabbixagent(
         value   => $hostname_real,
         require => File["${confdir}/zabbix_agentd.conf"],
       }
-      
+
       file { $homedir:
         ensure  => directory,
         source  => 'puppet:///modules/zabbixagent/win64',
@@ -126,4 +307,12 @@ class zabbixagent(
     }
     default: { notice "Unsupported operatingsystem  ${::operatingsystem}" }
   }
+}
+
+
+class { 'zabbixagent':
+  servers  => '10.0.20.16',
+  active_servers  => '10.0.20.16',
+  hostname => 'learn.cloudlab.cz',
+  version => '2'
 }
